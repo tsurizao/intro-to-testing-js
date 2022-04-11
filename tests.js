fe8cca58ad3969de3923fef61ef42393a2a8fe6b
Chase Martinez <chasemartinez22@gmail.com>
2022-04-11 16:05:47 -0500

    Updating with exercise #2 completion

--- tests.js
+++ tests.js
@@ -16,5 +16,8 @@ describe('helloWorld', function() {
 describe('sayHello', function(){
    it('should be a defined function', function(){
        expect(typeof sayHello).toBe('function');
-   })
+   });
+   it('should return a string when called', function(){
+       expect(typeof sayHello()).toBe('string');
+   });
 });
